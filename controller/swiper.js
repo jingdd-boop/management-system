38d6758938a54778ee336cfc20be5ef5edd54d86
jingdd-boop <1736929925@qq.com>
2020-07-26 08:59:51 +0800

    back

--- controller/swiper.js
+++ controller/swiper.js
@@ -36,4 +36,40 @@ router.get('/list', async (ctx, next) => {
   }
 })
 
+
+router.post('/upload',async(ctx,next)=>{
+ const fileid =  await cloudStorage.upload(ctx)
+ console.log(fileid)
+ //写数据库
+ const query = `
+ db.collection('swiper').add({
+   data:{
+     fileid: '${fileid}'
+   }
+})
+ `
+
+ const res = await callCloudDB(ctx,'databaseadd',query)
+ ctx.body = {
+   code:20000,
+   id_list: res.id_list
+ }
+})
+
+router.get('/del', async (ctx, next)=>{
+  const params = ctx.request.query
+  // 删除云数据库中的内容
+  const query = `db.collection('swiper').doc('${params._id}').remove()`
+  const delDBRes = await callCloudDB(ctx, 'databasedelete', query)
+
+  // 删除云存储中的文件
+  const delStorageRes = await cloudStorage.delete(ctx, [params.fileid])
+  ctx.body = {
+      code: 20000,
+      data: {
+          delDBRes,
+          delStorageRes,
+      }
+  }
+})
 module.exports = router
